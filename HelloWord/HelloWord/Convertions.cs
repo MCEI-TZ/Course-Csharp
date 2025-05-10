59aa4bf23a1e7c763d53c8dd3eecd65e2208d974
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-10 16:01:07 -0600

    feat(main): Add switch for call information of type data in C# and Conversion in C#(in process)

---
+++ HelloWord/HelloWord/Convertions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class Convertions
+    {
+        public void ConvertionData()
+        {
+            Console.WriteLine("Double a Int");
+            double miDouble = 13.37;
+            Console.WriteLine("Mi numero original double");
+            Console.WriteLine(miDouble);
+            int miInt;
+
+            // Casting
+            miInt = (int)miDouble;
+            Console.WriteLine("miInt = (int)miDouble;");
+            Console.WriteLine("Numero  convertido");
+            Console.WriteLine(miInt);
+
+            // -----------------------------------------
+            // Conversion Implicita
+            int num = 123456;
+            long numg = num;
+            Console.WriteLine(numg);
+
+            // -----------------------------------------
+            // Conversion tipo
+            string miString = miDouble.ToString();
+            Console.WriteLine(miString);
+
+            // ----------------------------------------------
+            // Parsing
+            int.Parse(miString);
+            Console.WriteLine(miString);
+        }
+    }
+}
--- HelloWord/HelloWord/Program.cs
+++ HelloWord/HelloWord/Program.cs
@@ -10,73 +10,38 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            //Console.ForegroundColor = ConsoleColor.Black;
-            //Console.BackgroundColor = ConsoleColor.Cyan;
-            //Console.Clear();
+            bool exit = false;
 
-            Console.WriteLine("Hello world!!!!");
-            // ---Types of Data in C#---
+            while (!exit)
+            {
+                Console.WriteLine("====================================");
+                Console.WriteLine("\tElige que datos quieres ver:\n");
+                Console.WriteLine("1.- Tipos de datos");
+                Console.WriteLine("2.- Conversion de Datos");
+                Console.WriteLine("3.- Salida:");
+                Console.WriteLine("Elige que opción:");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("====================================");
+                switch (opcion)
+                {
+                    case 1:
+                        new TypesData().TypeData();
+                        Console.WriteLine("====================================");
+                        break;
+                    case 2:
+                        new Convertions().ConvertionData();
+                        Console.WriteLine("====================================");
+                        break;
+                    case 3:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
 
-            // sbyte
-            sbyte num = 10;
-            Console.WriteLine("\t\nSbyte");
-            Console.WriteLine("Valor minimo: " + sbyte.MinValue);
-            Console.WriteLine("Valor maximo: " + sbyte.MaxValue);
+            }
 
-            // short
-            short num2 = 10;
-            Console.WriteLine("\t\nshort");
-            Console.WriteLine("Valor minimo: " + short.MinValue);
-            Console.WriteLine("Valor maximo: " + short.MaxValue);
-
-            // Integer
-            int num3 = 13;
-            Console.WriteLine("\t\nInteger");
-            Console.WriteLine("Valor minimo: " + int.MinValue);
-            Console.WriteLine("Valor maximo: " + int.MaxValue);
-
-            // Long
-            long numlong = 14;
-            Console.WriteLine("\t\nLong");
-            Console.WriteLine("Valor minimo: " + long.MinValue);
-            Console.WriteLine("Valor maximo: " + long.MaxValue);
-
-            // Float
-            float num4 = 10f;
-            Console.WriteLine("\t\nFloat");
-            Console.WriteLine("Valor minimo: " + float.MinValue);
-            Console.WriteLine("Valor maximo: " + float.MaxValue);
-
-            // Double
-            double num5 = 10.0;
-            Console.WriteLine("\t\nDouble");
-            Console.WriteLine("Valor minimo: " + double.MinValue);
-            Console.WriteLine("Valor maximo: " + double.MaxValue);
-
-            // Decimail
-            decimal num6 = 0;
-            Console.WriteLine("\t\nDecimal");
-            Console.WriteLine("Valor minimo: " + decimal.MinValue);
-            Console.WriteLine("Valor maximo: " + decimal.MaxValue);
-
-            // ----------------------------------------------------
-
-            // String
-            Console.WriteLine("\t\nString");
-            string message = "Como estas";
-
-            // Some methods
-            // Upper Case
-            string upperMessage = message.ToUpper();
-            // Lower Case
-            string lowerMessage = message.ToLower();
-            string withoutsplit = "Cadena; de; texto";
-            // Replace Method
-            withoutsplit = withoutsplit.Replace(";", "");
-            Console.WriteLine(message);
-            Console.WriteLine(upperMessage);
-            Console.WriteLine(lowerMessage);
-            Console.WriteLine(withoutsplit);
         }
     }
 }
