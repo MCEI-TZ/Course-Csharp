194d2a830cea68dccbb045dd8ca1eace90e7483f
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-11 19:10:35 -0600

    feat: Add examples of void methods and return methods

---
+++ HelloWord/HelloWord/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class Methods
+    {
+        // Void Method
+        public void WriteSomething()
+        {
+            Console.WriteLine("Something...");
+        }
+
+        // Void Method with Parameters
+        public void WriteSomething1(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        // Value Return Method
+        public string WriteSomething2() {
+            return "Method Return";
+        }
+
+        // Return Method with parameters
+        public int WriteSomething3(int num) { 
+            return num;
+        }
+    }
+}
