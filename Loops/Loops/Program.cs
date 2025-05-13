0dc851a2d82fda3238b7ca4b7d0ef54de247dcc2
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-13 08:38:47 -0600

    feat: Section Loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loop for
+            int numincrement = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numincrement = i;
+                Console.WriteLine(numincrement);
+            }
+
+
+        }
+    }
+}
