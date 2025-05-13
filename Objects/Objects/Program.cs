3bb738e315544373bd5b7cbae2bf842819a67524
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-13 10:11:09 -0600

    Add example of use of a Constructor with args

--- Objects/Objects/Program.cs
+++ Objects/Objects/Program.cs
@@ -18,6 +18,11 @@ namespace Objects
             humano.gender = "Male";
             humano.old = "13";
             humano.ShowHuman();
+
+            Console.WriteLine("=========================================");
+            // Constructor with args
+            Humano humano1 = new Humano("John", "Doe", "23", "Male");
+            humano1.ShowHuman();
         }
     }
 }
