b4027718fd7970756abfeea8b188e1b7f0f3dd5f
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-14 10:18:37 -0600

    feat: Add example of a simple a Array and the use of the propertie length

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -10,6 +10,18 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            // Array Simple
+            int[] numArray = new int[5];
+
+            numArray[0] = 1;
+            numArray[1] = 2;
+            numArray[2] = 3;
+            numArray[3] = 4;
+            numArray[4] = 5;
+
+            // Use of leng for know how many data have the array
+            Console.WriteLine(numArray.Length);
+
         }
     }
 }
