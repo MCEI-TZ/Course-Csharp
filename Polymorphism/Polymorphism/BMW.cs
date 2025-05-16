15d838a00948d2f7f5b3e2e081c2728436570b70
MCEI-TZ <144301860+MCEI-TZ@users.noreply.github.com>
2025-05-16 15:51:10 -0600

    Section polymorphism

---
+++ Polymorphism/Polymorphism/BMW.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BMW : Auto
+    {
+        public BMW()
+        {
+        }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public BMW(string marca, string modelo, int hP, string color) : base(hP, color)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        public override void ShowDetails()
+        {
+            Console.WriteLine("{0}-{1}-{2}-{3}", HP, Color, Marca, Modelo);
+        }
+
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Auto> autos = new List<Auto>()
+            {
+                new Auto(1,"Black"),
+                new Auto(2,"White"),
+                new Auto(3,"Black"),
+                new Auto(4,"Black")
+            };
+
+            Auto autos2 = new BMW("Califo","Mamas",1,"black");
+            autos2.ShowDetails();
+        }
+    }
+}
